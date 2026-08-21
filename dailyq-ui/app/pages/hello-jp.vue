c03a21c203d67f834a87a2263cb784b6900b921c
Adharsh M <48812285+adharshmk96@users.noreply.github.com>
2026-08-21 20:26:05 +0530

    chore: update

--- dailyq-ui/app/pages/hello-jp.vue
+++ dailyq-ui/app/pages/hello-jp.vue
@@ -7,7 +7,7 @@ useHead({
 <template>
   <main class="grid min-h-dvh place-items-center bg-default px-6 text-center">
     <p class="text-xl font-medium text-highlighted sm:text-2xl">
-      Hello JP, I'm just testing this deploy app by pushing.
+      Hello JP, I'm just testing this deploy app by pushing. Anything in git main deploys.
     </p>
   </main>
 </template>
